77673f86535be34728d07d584238ba5470dcb525
Hannah Pick <hannahpick@insitum.com>
2012-03-11 14:12:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HannahRails::Application.config.secret_token = '35261e4c276cf7599ec5f914e3f3a358cda9b82c5df367e346de1033d61909f3793e74f6abbe4b78ed15cd7935c109a19b3a41cc34fd3ed0a98f837e6bbc882f'
